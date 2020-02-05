504ae6a471095ab91039baabac75ac76a873963b
n1kko777 <3734489@gmail.com>
2020-02-05 19:09:54 +0500

    Add tests for listing users in django  admin

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, \
-                                    BaseUserManager, PermissionsMixin
+    BaseUserManager, PermissionsMixin
 
 
 class UserManager(BaseUserManager):
@@ -31,7 +31,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_stuff = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
