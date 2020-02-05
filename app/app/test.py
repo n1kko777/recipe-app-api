e3946307c2b154de22ad366741f0520d37671af0
n1kko777 <3734489@gmail.com>
2020-02-05 17:14:57 +0500

    Simple unit test with TDD

--- app/app/test.py
+++ app/app/test.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
-from app.calc import add
+from app.calc import add, subtract
+
 
 class CalcTests(TestCase):
     def test_add(self):
-        self.assertEqual(add(3,8), 11)
+        self.assertEqual(add(3, 8), 11)
 
+    def test_substract_numbers(self):
+        """Test that values are substracted and returned"""
+        self.assertEqual(subtract(5, 11), 6)
